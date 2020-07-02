6d89a22fc705be7b9838884ab7a85788a89dc17c
Julien Chiquet <julien.chiquet@gmail.com>
2020-07-02 16:18:50 +0200

    test for BipartiteSBM

---
+++ tests/testthat/test-BipartiteSBM_fit.R
@@ -0,0 +1,167 @@
+## Common parameters
+nbNodes  <- c(100, 120)
+blockProp <- list(row = c(.5, .5), col = c(1/3, 1/3, 1/3)) # group proportions
+nbBlocks <- sapply(blockProp, length)
+
+test_that("BipartiteSBM_fit 'Bernoulli' model, undirected, no covariate", {
+
+  ## BIPARTITE UNDIRECTED BERNOULLI SBM
+  means <- matrix(runif(6), 2, 3)  # connectivity matrix
+  connectParam <- list(mean = means)
+
+  ## Basic construction - check for wrong specifications
+  mySampler <- BipartiteSBM_sampler$new('bernoulli', nbNodes, blockProp, connectParam)
+
+  ## Construction----------------------------------------------------------------
+  mySBM <- BipartiteSBM_fit$new(mySampler$netMatrix, 'bernoulli')
+  expect_error(BipartiteSBM_fit$new(SamplerBernoulli$netMatrix, 'bernouilli'))
+
+  ## Checking class
+  expect_true(inherits(mySBM, "SBM"))
+  expect_true(inherits(mySBM, "SBM_fit"))
+  expect_true(inherits(mySBM, "BipartiteSBM_fit"))
+
+  ## Checking field access and format prior to estimation
+  ## parameters
+  expect_equal(mySBM$modelName, 'bernoulli')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, nbNodes)
+  expect_equal(mySBM$nbDyads, nbNodes[1]*nbNodes[2])
+  expect_true(is.na(mySBM$connectParam$mean))
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
+  ## Estimation-----------------------------------------------------------------
+  mySBM$optimize(verbosity = 0, nbCores = 1)
+
+  ## Expectation
+  expect_equal(dim(mySBM$expectation), nbNodes)
+  expect_true(all(mySBM$expectation >= 0, na.rm = TRUE))
+  expect_true(all(mySBM$expectation <= 1, na.rm = TRUE))
+  expect_null(mySBM$connectParam$var)
+
+  ## blocks
+  expect_equal(mySBM$nbBlocks, nbBlocks)
+  expect_equivalent(dim(mySBM$probMemberships[[1]]), c(nbNodes[1], nbBlocks[1]))
+  expect_equivalent(dim(mySBM$probMemberships[[2]]), c(nbNodes[2], nbBlocks[2]))
+  expect_equal(sort(unique(mySBM$memberships[[1]])), 1:nbBlocks[1])
+  expect_equal(sort(unique(mySBM$memberships[[2]])), 1:nbBlocks[2])
+
+  ## correctness
+  expect_lt(rmse(sort(mySBM$connectParam$mean), sort(means)), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[1]], mySampler$memberships[[1]]), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[2]], mySampler$memberships[[2]]), 1e-1)
+
+})
+
+test_that("BipartiteSBM_fit 'Poisson' model, undirected, no covariate", {
+
+  ## SIMPLE DIRECTED POISSON SBM
+  means <- matrix(rbinom(6, 30, 0.25), 2, 3)  # connectivity matrix
+  connectParam <- list(mean = means)
+
+  ## Basic construction - check for wrong specifications
+  mySampler <- BipartiteSBM_sampler$new('poisson', nbNodes, blockProp, connectParam)
+
+  ## Construction----------------------------------------------------------------
+  mySBM <- BipartiteSBM_fit$new(mySampler$netMatrix, 'poisson')
+  expect_error(BipartiteSBM_fit$new(SamplerBernoulli$netMatrix, 'poison'))
+
+  ## Checking class
+  expect_true(inherits(mySBM, "SBM"))
+  expect_true(inherits(mySBM, "SBM_fit"))
+  expect_true(inherits(mySBM, "BipartiteSBM_fit"))
+
+  ## Checking field access and format prior to estimation
+  ## parameters
+  expect_equal(mySBM$modelName, 'poisson')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, nbNodes)
+  expect_equal(mySBM$nbDyads, nbNodes[1]*nbNodes[2])
+  expect_true(is.na(mySBM$connectParam$mean))
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
+  ## Estimation-----------------------------------------------------------------
+  mySBM$optimize(verbosity = 0, nbCores = 1)
+
+  ## Expectation
+  expect_equal(dim(mySBM$expectation), nbNodes)
+  expect_true(all(mySBM$expectation >= 0, na.rm = TRUE))
+  expect_null(mySBM$connectParam$var)
+
+  ## blocks
+  expect_equal(mySBM$nbBlocks, nbBlocks)
+  expect_equivalent(dim(mySBM$probMemberships[[1]]), c(nbNodes[1], nbBlocks[1]))
+  expect_equivalent(dim(mySBM$probMemberships[[2]]), c(nbNodes[2], nbBlocks[2]))
+  expect_equal(sort(unique(mySBM$memberships[[1]])), 1:nbBlocks[1])
+  expect_equal(sort(unique(mySBM$memberships[[2]])), 1:nbBlocks[2])
+
+  ## correctness
+  expect_lt(rmse(sort(mySBM$connectParam$mean), sort(means)), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[1]], mySampler$memberships[[1]]), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[2]], mySampler$memberships[[2]]), 1e-1)
+
+})
+
+test_that("BipartiteSBM_fit 'Gaussian' model, undirected, no covariate", {
+
+  ## SIMPLE UNDIRECTED GAUSSIAN SBM
+  means <- 20 * matrix(runif(6), 2, 3)  # connectivity matrix
+  connectParam <- list(mean = means, var = 1)
+
+  ## Basic construction - check for wrong specifications
+  mySampler <- BipartiteSBM_sampler$new('gaussian', nbNodes, blockProp, connectParam)
+
+  ## Construction----------------------------------------------------------------
+  mySBM <- BipartiteSBM_fit$new(mySampler$netMatrix, 'gaussian')
+  expect_error(BipartiteSBM_fit$new(SamplerBernoulli$netMatrix, 'groß'))
+
+  ## Checking class
+  expect_true(inherits(mySBM, "SBM"))
+  expect_true(inherits(mySBM, "SBM_fit"))
+  expect_true(inherits(mySBM, "BipartiteSBM_fit"))
+
+  ## Checking field access and format prior to estimation
+  ## parameters
+  expect_equal(mySBM$modelName, 'gaussian')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, nbNodes)
+  expect_equal(mySBM$nbDyads, nbNodes[1]*nbNodes[2])
+  expect_true(is.na(mySBM$connectParam$mean))
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
+  ## Estimation-----------------------------------------------------------------
+  mySBM$optimize(verbosity = 0, nbCores = 1)
+
+  ## Expectation
+  expect_equal(dim(mySBM$expectation), nbNodes)
+  expect_gt(mySBM$connectParam$var, 0)
+
+  ## blocks
+  expect_equal(mySBM$nbBlocks, nbBlocks)
+  expect_equivalent(dim(mySBM$probMemberships[[1]]), c(nbNodes[1], nbBlocks[1]))
+  expect_equivalent(dim(mySBM$probMemberships[[2]]), c(nbNodes[2], nbBlocks[2]))
+  expect_equal(sort(unique(mySBM$memberships[[1]])), 1:nbBlocks[1])
+  expect_equal(sort(unique(mySBM$memberships[[2]])), 1:nbBlocks[2])
+
+  ## correctness
+  expect_lt(rmse(sort(mySBM$connectParam$mean), sort(means)), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[1]], mySampler$memberships[[1]]), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships[[2]], mySampler$memberships[[2]]), 1e-1)
+
+})
+
--- tests/testthat/test-BipartiteSBM_sampler.R
+++ tests/testthat/test-BipartiteSBM_sampler.R
@@ -1,53 +1,60 @@
 ## Common parameters
-nbNodes  <- 90
-nbBlocks <- 3
-blockProp <- c(.5, .25, .25) # group proportions
+nbNodes  <- c(100, 120)
+blockProp <- list(row = c(.5, .5), col = c(1/3, 1/3, 1/3)) # group proportions
+nbBlocks <- sapply(blockProp, length)
 
-test_that("Construction, fields access and other basics work in class SimpleSBM_Sampler (undirected Bernoulli model, no covariate)", {
+test_that("Construction, fields access and other basics work in class BipartiteSBM_Sampler (undirected Bernoulli model, no covariate)", {
 
-  ## SIMPLE UNDIRECTED BERNOULLI SBM
-  means <- diag(.4, 3) + 0.05
+  ## BIPARTITE UNDIRECTED BERNOULLI SBM
+  means <- matrix(runif(6), 2, 3)  # connectivity matrix
   connectParam <- list(mean = means)
 
   ## Basic construction - check for wrong specifications
-  mySampler <- SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, connectParam)
-  expect_error(SimpleSBM_sampler$new('bernouilli',nbNodes, FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('bernoulli', -10    , FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('bernoulli', c(1,2) , FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, -2, connectParam))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE,  c(0,1), connectParam))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, list(mean = matrix( 2, nbBlocks, nbBlocks))))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, list(mean = matrix(-2, nbBlocks, nbBlocks))))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, list(mean = matrix(runif(nbBlocks**2), nbBlocks, nbBlocks))))
-  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, list(mean = matrix(0, nbBlocks - 1, nbBlocks))))
+  mySampler <- BipartiteSBM_sampler$new('bernoulli', nbNodes, blockProp, connectParam)
+  expect_error(SimpleSBM_sampler$new('bernouilli',nbNodes, blockProp, connectParam))
+  expect_error(SimpleSBM_sampler$new('bernoulli', -10    , blockProp, connectParam))
+  expect_error(SimpleSBM_sampler$new('bernoulli', c(1,2) , blockProp, connectParam))
+  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, 2, connectParam))
+  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, c(0,1), connectParam))
+  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, blockProp, list(mean = matrix( 2, nbBlocks[1], nbBlocks[2]))))
+  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, blockProp, list(mean = matrix(-2, nbBlocks[1], nbBlocks[2]))))
+  expect_error(SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, list(mean = matrix(0, nbBlocks[1] - 1, nbBlocks[2]))))
 
   ## Checking class
   expect_true(inherits(mySampler, "SBM"))
   expect_true(inherits(mySampler, "SBM_sampler"))
-  expect_true(inherits(mySampler, "SimpleSBM_sampler"))
+  expect_true(inherits(mySampler, "BipartiteSBM_sampler"))
 
   ## Checking field access and format
 
   ## parameters
-  expect_equal(mySampler$modelName, 'bernoulli')
-  expect_equal(mySampler$nbNodes, nbNodes)
-  expect_equal(mySampler$dimension, c(nbNodes, nbNodes))
-  expect_equal(mySampler$nbDyads, nbNodes*(nbNodes - 1)/2)
+  expect_equal(mySBM$modelName, 'bernoulli')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, nbNodes)
+  expect_equal(mySBM$nbDyads, nbNodes[1]*nbNodes[2])
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
   expect_equal(mySampler$connectParam$mean, means)
   expect_null(mySampler$connectParam$var)
-  expect_equal(dim(mySampler$expectation), c(nbNodes, nbNodes))
+  expect_equal(dim(mySampler$expectation), nbNodes)
   expect_true(all(mySampler$expectation >= 0, na.rm = TRUE))
   expect_true(all(mySampler$expectation <= 1, na.rm = TRUE))
-  expect_true(isSymmetric(mySampler$netMatrix))
-  expect_true(all(is.na(diag(mySampler$netMatrix))))
-  expect_true(!mySampler$directed)
+  expect_false(isSymmetric(mySampler$netMatrix))
 
   ## blocks
   expect_equal(mySampler$blockProp, blockProp)
   expect_equal(mySampler$nbBlocks, nbBlocks)
-  expect_equal(dim(mySampler$indMemberships), c(nbNodes, nbBlocks))
-  expect_equal(sort(unique(mySampler$memberships)), 1:nbBlocks)
-  expect_equal(length(mySampler$memberships), nbNodes)
+  expect_equivalent(dim(mySampler$indMemberships[[1]]), c(nbNodes[1], nbBlocks[1]))
+  expect_equivalent(dim(mySampler$indMemberships[[2]]), c(nbNodes[2], nbBlocks[2]))
+  expect_equal(sort(unique(mySampler$memberships[[1]])), 1:nbBlocks[1])
+  expect_equal(sort(unique(mySampler$memberships[[2]])), 1:nbBlocks[2])
+  expect_equal(length(mySampler$memberships[[1]]), nbNodes[1])
+  expect_equal(length(mySampler$memberships[[2]]), nbNodes[2])
 
   ## covariates
   expect_equal(mySampler$nbCovariates, 0)
@@ -56,7 +63,7 @@ test_that("Construction, fields access and other basics work in class SimpleSBM_
 
 })
 
-test_that("Construction, fields access and other basics work in class SimpleSBM_Sampler (directed Bernoulli model, no covariate)", {
+test_that("Construction, fields access and other basics work in class BipartiteSBM_Sampler (directed Bernoulli model, no covariate)", {
 
   ## SIMPLE DIRECTED BERNOULLI SBM
   means <- matrix(runif(nbBlocks**2), nbBlocks, nbBlocks)
@@ -112,53 +119,51 @@ test_that("Construction, fields access and other basics work in class SimpleSBM_
 test_that("Construction, fields access and other basics work in class SimpleSBM_Sampler (undirected Poisson model, no covariate)", {
 
   ## SIMPLE UNDIRECTED POISSON SBM
-  means <- diag(15., 3) + 5
+  means <- matrix(rbinom(6, 30, 0.25), 2, 3)  # connectivity matrix
   connectParam <- list(mean = means)
 
   ## Basic construction - check for wrong specifications
-  mySampler <- SimpleSBM_sampler$new('poisson', nbNodes, FALSE, blockProp, connectParam)
-  expect_error(SimpleSBM_sampler$new('poison' , nbNodes, FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('poisson', -10    , FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('poisson', c(1,2) , FALSE, blockProp, connectParam))
-  expect_error(SimpleSBM_sampler$new('poisson', nbNodes, FALSE, -2, connectParam))
-  expect_error(SimpleSBM_sampler$new('poisson', nbNodes, FALSE,  c(0,1), connectParam))
-  expect_error(SimpleSBM_sampler$new('poisson', nbNodes, FALSE, blockProp, list(mean = matrix(-2, nbBlocks, nbBlocks))))
-  expect_error(SimpleSBM_sampler$new('poisson', nbNodes, FALSE, blockProp, list(mean = round(40 * matrix(runif(nbBlocks**2), nbBlocks, nbBlocks)))))
-  expect_error(SimpleSBM_sampler$new('poisson', nbNodes, FALSE, blockProp, list(mean = matrix(2 , nbBlocks - 1, nbBlocks))))
+  mySampler <- BipartiteSBM_sampler$new('poisson', nbNodes, blockProp, connectParam)
+  expect_error(BipartiteSBM_sampler$new('poison' , nbNodes, blockProp, connectParam))
+  expect_error(BipartiteSBM_sampler$new('poisson', -10    , blockProp, connectParam))
+  expect_error(BipartiteSBM_sampler$new('poisson', nbNodes, -2, connectParam))
+  expect_error(BipartiteSBM_sampler$new('poisson', nbNodes,  c(0,1), connectParam))
+  expect_error(BipartiteSBM_sampler$new('poisson', nbNodes, blockProp, list(mean = matrix(-2, nbBlocks[1], nbBlocks[2]))))
+  expect_error(BipartiteSBM_sampler$new('poisson', nbNodes, blockProp, list(mean = matrix(2 , nbBlocks[1] - 1, nbBlocks[2]))))
 
   ## Checking class
   expect_true(inherits(mySampler, "SBM"))
   expect_true(inherits(mySampler, "SBM_sampler"))
-  expect_true(inherits(mySampler, "SimpleSBM_sampler"))
+  expect_true(inherits(mySampler, "BipartiteSBM_sampler"))
 
   ## Checking field access and format
 
   ## parameters
   expect_equal(mySampler$modelName, 'poisson')
   expect_equal(mySampler$nbNodes, nbNodes)
-  expect_equal(mySampler$dimension, c(nbNodes, nbNodes))
-  expect_equal(mySampler$nbDyads, nbNodes*(nbNodes - 1)/2)
+  expect_equal(mySampler$dimension, nbNodes)
+  expect_equal(mySampler$nbDyads, nbNodes[1]*nbNodes[2])
   expect_equal(mySampler$connectParam$mean, means)
   expect_null(mySampler$connectParam$var)
-  expect_equal(dim(mySampler$expectation), c(nbNodes, nbNodes))
+  expect_equal(dim(mySampler$expectation), nbNodes)
   expect_true(all(mySampler$expectation >= 0, na.rm = TRUE))
-  expect_true(all(is.na(diag(mySampler$netMatrix))))
-  expect_true(isSymmetric(mySampler$netMatrix))
+  expect_false(isSymmetric(mySampler$netMatrix))
 
   ## blocks
   expect_equal(mySampler$blockProp, blockProp)
   expect_equal(mySampler$nbBlocks, nbBlocks)
-  expect_equal(dim(mySampler$indMemberships), c(nbNodes, nbBlocks))
-  expect_equal(sort(unique(mySampler$memberships)), 1:nbBlocks)
-  expect_equal(length(mySampler$memberships), nbNodes)
+  expect_equivalent(dim(mySampler$indMemberships[[1]]), c(nbNodes[1], nbBlocks[1]))
+  expect_equivalent(dim(mySampler$indMemberships[[2]]), c(nbNodes[2], nbBlocks[2]))
+  expect_equal(sort(unique(mySampler$memberships[[1]])), 1:nbBlocks[1])
+  expect_equal(sort(unique(mySampler$memberships[[2]])), 1:nbBlocks[2])
+  expect_equal(length(mySampler$memberships[[1]]), nbNodes[1])
+  expect_equal(length(mySampler$memberships[[2]]), nbNodes[2])
 
   ## covariates
   expect_equal(mySampler$nbCovariates, 0)
   expect_equal(mySampler$covarList, list())
   expect_equal(mySampler$covarParam, numeric(0))
-
 })
-
 
 test_that("Construction, fields access and other basics work in class SimpleSBM_Sampler (directed Poisson model, no covariate)", {
 
