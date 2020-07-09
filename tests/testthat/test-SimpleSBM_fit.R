66d83dc52c1d112b588415d6c93532f51c9979ad
Julien Chiquet <julien.chiquet@gmail.com>
2020-07-09 14:10:59 +0200

    added tests for directed Bernoulli, Poisson and Gaussian Simple SBM

--- tests/testthat/test-SimpleSBM_fit.R
+++ tests/testthat/test-SimpleSBM_fit.R
@@ -16,9 +16,9 @@ test_that("SimpleSBM_fit 'Bernoulli' model, undirected, no covariate", {
 
   ## Construction----------------------------------------------------------------
   mySBM <- SimpleSBM_fit$new(mySampler$netMatrix, 'bernoulli', FALSE)
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'bernouilli', FALSE))
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix[1:20, 1:30], 'bernouilli', FALSE))
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'bernoulli', TRUE))
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix, 'bernouilli', FALSE))
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix[1:20, 1:30], 'bernouilli', FALSE))
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix, 'bernoulli', TRUE))
 
   ## Checking class
   expect_true(inherits(mySBM, "SBM"))
@@ -67,17 +67,17 @@ test_that("SimpleSBM_fit 'Bernoulli' model, undirected, no covariate", {
 test_that("SimpleSBM_fit 'Bernoulli' model, directed, no covariate", {
 
   ## SIMPLE UNDIRECTED BERNOULLI SBM
-  means <- diag(.4, 3) + 0.05
+  means <- matrix(c(1:9)/10, 3,  3)
   connectParam <- list(mean = means)
 
   ## Basic construction - check for wrong specifications
-  mySampler <- SimpleSBM_sampler$new('bernoulli', nbNodes, FALSE, blockProp, connectParam)
+  mySampler <- SimpleSBM_sampler$new('bernoulli', nbNodes, TRUE, blockProp, connectParam)
 
   ## Construction----------------------------------------------------------------
-  mySBM <- SimpleSBM_fit$new(mySampler$netMatrix, 'bernoulli', FALSE)
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'bernouilli', FALSE))
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix[1:20, 1:30], 'bernouilli', FALSE))
-  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'bernoulli', TRUE))
+  mySBM <- SimpleSBM_fit$new(mySampler$netMatrix, 'bernoulli', TRUE)
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix, 'bernouilli', TRUE))
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix, 'bernoulli', FALSE))
+  expect_error(SimpleSBM_fit$new(mySampler$netMatrix[1:20, 1:30], 'bernouilli', FALSE))
 
   ## Checking class
   expect_true(inherits(mySBM, "SBM"))
@@ -89,10 +89,10 @@ test_that("SimpleSBM_fit 'Bernoulli' model, directed, no covariate", {
   expect_equal(mySBM$modelName, 'bernoulli')
   expect_equal(mySBM$nbNodes, nbNodes)
   expect_equal(mySBM$dimension, c(nbNodes, nbNodes))
-  expect_equal(mySBM$nbDyads, nbNodes*(nbNodes - 1)/2)
+  expect_equal(mySBM$nbDyads, nbNodes*(nbNodes - 1))
   expect_true(all(is.na(diag(mySBM$netMatrix))))
-  expect_true(isSymmetric(mySBM$netMatrix))
-  expect_true(!mySBM$directed)
+  expect_true(!isSymmetric(mySBM$netMatrix))
+  expect_true(mySBM$directed)
   expect_true(is.na(mySBM$connectParam$mean))
 
   ## covariates
@@ -118,8 +118,8 @@ test_that("SimpleSBM_fit 'Bernoulli' model, directed, no covariate", {
   expect_equal(length(mySBM$memberships), nbNodes)
 
   ## correctness
-  expect_lt(rmse(mySBM$connectParam$mean, means), 0.25)
-  expect_lt(1 - aricode::ARI(mySBM$memberships, mySampler$memberships), 0.25)
+  expect_lt(rmse(sort(mySBM$connectParam$mean), means), 0.2)
+  expect_lt(1 - aricode::ARI(mySBM$memberships, mySampler$memberships), 0.2)
 
 })
 
@@ -181,6 +181,65 @@ test_that("SimpleSBM_fit 'Poisson' model, undirected, no covariate", {
 
 })
 
+test_that("SimpleSBM_fit 'Poisson' model, directed, no covariate", {
+
+  ## SIMPLE UNDIRECTED BERNOULLI SBM
+  means <- matrix(1:9, 3,  3)
+  connectParam <- list(mean = means)
+
+  ## Basic construction - check for wrong specifications
+  mySampler <- SimpleSBM_sampler$new('poisson', nbNodes, TRUE, blockProp, connectParam)
+
+  ## Construction----------------------------------------------------------------
+  mySBM <- SimpleSBM_fit$new(mySampler$netMatrix, 'poisson', TRUE)
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'poison', TRUE))
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix[1:20, 1:30], 'poisson', FALSE))
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'poisson', FALSE))
+
+  ## Checking class
+  expect_true(inherits(mySBM, "SBM"))
+  expect_true(inherits(mySBM, "SBM_fit"))
+  expect_true(inherits(mySBM, "SimpleSBM_fit"))
+
+  ## Checking field access and format prior to estimation
+  ## parameters
+  expect_equal(mySBM$modelName, 'poisson')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, c(nbNodes, nbNodes))
+  expect_equal(mySBM$nbDyads, nbNodes*(nbNodes - 1))
+  expect_true(all(is.na(diag(mySBM$netMatrix))))
+  expect_true(!isSymmetric(mySBM$netMatrix))
+  expect_true(mySBM$directed)
+  expect_true(is.na(mySBM$connectParam$mean))
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
+  ## Estimation-----------------------------------------------------------------
+  mySBM$optimize(verbosity = 0)
+  mySBM$setModel(3)
+
+  ## Expectation
+  expect_equal(dim(mySBM$expectation), c(nbNodes, nbNodes))
+  expect_true(all(mySampler$expectation >= 0, na.rm = TRUE))
+  expect_null(mySBM$connectParam$var)
+
+  ## blocks
+  expect_equal(mySBM$nbBlocks, nbBlocks)
+  expect_equal(dim(mySBM$probMemberships), c(nbNodes, nbBlocks))
+  expect_equal(sort(unique(mySBM$memberships)), 1:nbBlocks)
+  expect_equal(length(mySBM$memberships), nbNodes)
+
+  ## correctness
+  expect_lt(rmse(sort(mySBM$connectParam$mean), means), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships, mySampler$memberships), 1e-1)
+
+})
+
+
 test_that("SimpleSBM_fit 'Gaussian' model, undirected, no covariate", {
 
   ## SIMPLE UNDIRECTED GAUSSIAN SBM
@@ -238,3 +297,63 @@ test_that("SimpleSBM_fit 'Gaussian' model, undirected, no covariate", {
 
 })
 
+
+
+
+
+test_that("SimpleSBM_fit 'Gaussian' model, undirected, no covariate", {
+
+  ## SIMPLE UNDIRECTED GAUSSIAN SBM
+  means <- matrix(1:9,3,3)
+  connectParam <- list(mean = means, var = 2)
+
+  ## Basic construction - check for wrong specifications
+  mySampler <- SimpleSBM_sampler$new('gaussian', nbNodes, TRUE, blockProp, connectParam)
+
+  ## Construction----------------------------------------------------------------
+  mySBM <- SimpleSBM_fit$new(mySampler$netMatrix, 'gaussian', TRUE)
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'normal', TRUE))
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix[1:20, 1:30], 'gaussian', TRUE))
+  expect_error(SimpleSBM_fit$new(SamplerBernoulli$netMatrix, 'gaussian', FALSE))
+
+  ## Checking class
+  expect_true(inherits(mySBM, "SBM"))
+  expect_true(inherits(mySBM, "SBM_fit"))
+  expect_true(inherits(mySBM, "SimpleSBM_fit"))
+
+  ## Checking field access and format prior to estimation
+  ## parameters
+  expect_equal(mySBM$modelName, 'gaussian')
+  expect_equal(mySBM$nbNodes, nbNodes)
+  expect_equal(mySBM$dimension, c(nbNodes, nbNodes))
+  expect_equal(mySBM$nbDyads, nbNodes*(nbNodes - 1))
+  expect_true(all(is.na(diag(mySBM$netMatrix))))
+  expect_true(!isSymmetric(mySBM$netMatrix))
+  expect_true(mySBM$directed)
+  expect_true(is.na(mySBM$connectParam$mean))
+
+  ## covariates
+  expect_null(mySBM$covarExpect)
+  expect_equal(mySBM$nbCovariates, 0)
+  expect_equal(mySBM$covarList, list())
+  expect_equal(mySBM$covarParam, numeric(0))
+
+  ## Estimation-----------------------------------------------------------------
+  mySBM$optimize(verbosity = 0)
+  mySBM$setModel(3)
+
+  ## Expectation
+  expect_equal(dim(mySBM$expectation), c(nbNodes, nbNodes))
+  expect_gt(mySBM$connectParam$var, 0)
+
+  ## blocks
+  expect_equal(mySBM$nbBlocks, nbBlocks)
+  expect_equal(dim(mySBM$probMemberships), c(nbNodes, nbBlocks))
+  expect_equal(sort(unique(mySBM$memberships)), 1:nbBlocks)
+  expect_equal(length(mySBM$memberships), nbNodes)
+
+  ## correctness
+  expect_lt(rmse(sort(mySBM$connectParam$mean), means), 1e-1)
+  expect_lt(1 - aricode::ARI(mySBM$memberships, mySampler$memberships), 1e-1)
+
+})
