3e62c3f47dfe9679759f1016409cf522dd3fc45e
Louis <louis.lacoste@agroparistech.fr>
2026-04-02 15:22:54 +0200

    Reimplementing reorder for bipartite with nodes covariates capabilities

--- R/R6Class-BipartiteSBM_fit.R
+++ R/R6Class-BipartiteSBM_fit.R
@@ -140,10 +140,28 @@ BipartiteSBM_fit <-
       },
       #' @description permute group labels by order of decreasing probability
       reorder = function() {
-        oRow <- order(private$theta$mean %*% private$pi[[2]], decreasing = TRUE)
-        oCol <- order(private$pi[[1]] %*% private$theta$mean, decreasing = TRUE)
-        private$pi[[1]] <- private$pi[[1]][oRow]
-        private$pi[[2]] <- private$pi[[2]][oCol]
+        if (self$nbNodesCovariates["col"] > 0 && self$nbBlocks[2] >= 2L) {
+          order_pi_col <- colMeans(private$pi[[2]])
+        } else {
+          order_pi_col <- private$pi[[2]]
+        }
+        if (self$nbNodesCovariates["row"] > 0 && self$nbBlocks[1] >= 2L) {
+          order_pi_row <- colMeans(private$pi[[1]])
+        } else {
+          order_pi_row <- private$pi[[1]]
+        }
+        oRow <- order(private$theta$mean %*% order_pi_col, decreasing = TRUE)
+        oCol <- order(order_pi_row %*% private$theta$mean, decreasing = TRUE)
+        if (self$nbNodesCovariates["row"] > 0 && self$nbBlocks[1] >= 2L) {
+          private$pi[[1]] <- private$pi[[1]][, oRow, drop = FALSE]
+        } else {
+          private$pi[[1]] <- private$pi[[1]][oRow]
+        }
+        if (self$nbNodesCovariates["col"] > 0 && self$nbBlocks[2] >= 2L) {
+          private$pi[[2]] <- private$pi[[2]][,oCol, drop = FALSE]
+        } else {
+          private$pi[[2]] <- private$pi[[2]][oCol]
+        }
         private$theta$mean <- private$theta$mean[oRow, oCol, drop = FALSE]
         private$Z[[1]] <- private$Z[[1]][, oRow, drop = FALSE]
         private$Z[[2]] <- private$Z[[2]][, oCol, drop = FALSE]
